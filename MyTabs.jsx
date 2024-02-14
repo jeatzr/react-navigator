4cb22381b0117d23e6601220b1ada38f3d70bd75
jeatzr <profesor.jeatienzar@iesazarquiel.es>
2024-02-14 12:23:01 +0100

    Fine Tunning

--- MyTabs.jsx
+++ MyTabs.jsx
@@ -2,15 +2,48 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import ScreenTwo from './screens/ScreenTwo';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 export default function MyTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Another" component={ScreenTwo} />
-      <Tab.Screen name="Setrtings" component={SettingsScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: 'green',
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" size={size} color={color} />
+          ),
+          headerShown: false
+        }}
+      />
+      <Tab.Screen
+        name="Another"
+        component={ScreenTwo}
+        options={{
+          tabBarLabel: 'Community',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account-heart" size={size} color={color} />
+          ),
+          tabBarBadge: 3
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: 'Settings',
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="settings" size={size} color={color} />
+          )
+        }} />
     </Tab.Navigator>
   );
 }
